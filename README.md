b3362c03de6c0d67865da9d6e8ece0f4c336f8b6
saurabh shandilya <saurabhshandilya.1991@gmail.com>
2018-10-09 21:39:00 +0530

    1. updated Readme. 2. applied expressjs.com security practices.

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # expressJsProductionTemplate
+
+Compilation of a few standard practices for deploying ExpressJs app to production.
+1. Rather than this relative style of adding to your routes, use absolute with dirname.
+- var indexRouter = require('./routes/index');                             
+- var indexRouter = require(path.join(__dirname,'routes','index'));
+
+2. Handle exceptions/Error properly
+- Make a global error handler and throw errors from all the other middlewares or functions so that this error handler sends the appropriate response.
+- Handle uncaught exception by process and get alert if that happens.  Exiting on process Exception is encouraged.  
+- Domains & 'process uncaught exception', both are discouraged by expressjs.com in their What Not To Do.
+- Use promises or try/catch in case of async or sync codes for error handling.
+
+3. Use gzip compression
+- This is achieved by using compression module, with default params other than the filter function made custom. 
+
+4. Don’t use synchronous functions at all. 
+
+5. Do logging correctly.
+
+6. Set NODE_ENV to “production”.
+- Setting NODE_ENV to “production” makes Express:
+- Cache view templates.
+- Cache CSS files generated from CSS extensions.
+- Generate less verbose error messages.
+- Tests indicate that just doing this can improve app performance by a factor of three!
+
+7. Structure of code folders itself.
+8. Follow security best practices by Expressjs.com by using Helmet.
+
+
+
+
+
+
+Sources:
+1. https://expressjs.com/en/advanced/best-practice-performance.html
+2. https://evanhahn.com/express-dot-static-deep-dive/
+3. https://strongloop.com/strongblog/robust-node-applications-error-handling/
+4. https://www.joyent.com/node-js/production/design/errors
+5. https://www.dynatrace.com/news/blog/the-drastic-effects-of-omitting-node_env-in-your-express-js-applications/
+6. https://github.com/expressjs/compression
+
+
+
--- app.js
+++ app.js
@@ -1,20 +1,62 @@
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require('http-errors');
+var helmet = require('helmet');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var path = require('path');
+var express = require('express');
+var logger = require('morgan');
+var compression = require('compression');
+
+var indexRouter = require(path.join(__dirname,'routes','index'));
+var usersRouter = require(path.join(__dirname,'routes','users'));
 
 var app = express();
+
+app.use(compression({filter: shouldCompress}));
+app.use(helmet());
+
+function shouldCompress (req, res) {
+    console.log("compared!");
+  if (req.headers['x-no-compression']) {
+    // Make some case when you don't need compression i.e via matching up some header and using compression middleware accordingly.
+    return false;
+  }
+  else{
+    return compression.filter(req, res);
+  }
+};
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    // serve some diff. 404 page or send this to error handler.
+    // next(createError(404));
+    res.status(404);
+    return res.send();
+});
+  
+  // error handler
+app.use(function(err, req, res, next) {
+// set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    return res.json({ message: error.message });
+});
+  
+process.on('uncaughtException', function (er) {
+    // do something with this error.  Send a mail to yourself or admins telling the cause.
+    console.error(er.stack)
+    process.exit(1)
+});
 
 module.exports = app;
